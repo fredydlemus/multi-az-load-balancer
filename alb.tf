ee3921668e0fbd456a953cf1d28689a91f631a1d
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2025-10-17 20:07:35 -0600

    add elb target group and associate listener

--- alb.tf
+++ alb.tf
@@ -9,3 +9,34 @@ resource "aws_lb" "app" {
 
   tags = local.tags
 }
+
+resource "aws_lb_target_group" "app_tg" {
+  name = "${local.name}-app-tg"
+  vpc_id = module.vpc.vpc_id
+  port = 80
+  protocol = "HTTP"
+  target_type = "instance"
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200-302"
+    interval = 15
+    timeout = 5
+    healthy_threshold = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = local.tags
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
