ab7031c8d0a335c6e70e75be7c74c2a856e3f697
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2025-10-19 07:19:34 -0600

    pass tf format

--- alb.tf
+++ alb.tf
@@ -13,19 +13,19 @@ resource "aws_lb" "app" {
 
 # Target Group for the Application Load Balancer
 resource "aws_lb_target_group" "app_tg" {
-  name = "${local.name}-app-tg"
-  vpc_id = module.vpc.vpc_id
-  port = 80
-  protocol = "HTTP"
+  name        = "${local.name}-app-tg"
+  vpc_id      = module.vpc.vpc_id
+  port        = 80
+  protocol    = "HTTP"
   target_type = "instance"
 
   health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200-302"
-    interval = 15
-    timeout = 5
-    healthy_threshold = 2
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-302"
+    interval            = 15
+    timeout             = 5
+    healthy_threshold   = 2
     unhealthy_threshold = 3
   }
 
@@ -35,11 +35,11 @@ resource "aws_lb_target_group" "app_tg" {
 # Listener for the Application Load Balancer
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app.arn
-  port = 80
-  protocol = "HTTP"
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
