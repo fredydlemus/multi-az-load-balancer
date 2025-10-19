2536e58620f559e3138d1cb02a4e6dc50b38cd78
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2025-10-19 07:31:21 -0600

    add ASG

---
+++ asg.tf
@@ -0,0 +1,64 @@
+# Amazon Linux 2023 AMI
+data "aws_ami" "al2023" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Launch template for the ASG
+resource "aws_launch_template" "web" {
+  name_prefix            = "${local.name}-web-"
+  image_id               = data.aws_ami.al2023.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.asg_sg.id]
+
+  user_data = base64encode(file("user-data.sh"))
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = local.tags
+  }
+}
+
+# Auto Scaling Group
+resource "aws_autoscaling_group" "web_asg" {
+  name                      = "${local.name}-web-asg"
+  desired_capacity          = 2
+  max_size                  = 3
+  min_size                  = 1
+  vpc_zone_identifier       = module.vpc.private_subnets
+  health_check_type         = "ELB"
+  health_check_grace_period = 60
+
+  target_group_arns = [aws_lb_target_group.app_tg.arn]
+
+  launch_template {
+    id      = aws_launch_template.web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${local.name}-web-asg"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
